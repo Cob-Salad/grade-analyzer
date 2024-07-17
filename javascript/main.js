e967b70a6fd99d172d562b34e47d4fe79b6d50db
Colby Jensen <endergdawg@gmail.com>
2024-07-17 11:06:08 -0600

    add semicolons

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 
 
 
-const input = [75, 88, 37, 100, 100, 89, 92, 93, 68, 76, 85, 90, 40, 99]
+const input = [75, 88, 37, 100, 100, 89, 92, 93, 68, 76, 85, 90, 40, 99];
 
 
 const gradeAnalysis = () => {
@@ -15,20 +15,20 @@ const gradeAnalysis = () => {
     }
     
     for (let i = 0; i < input.length; i++) {
-        scoreObject.total += input[i]
-        scoreObject.average = scoreObject.total/input.length
+        scoreObject.total += input[i];
+        scoreObject.average = scoreObject.total/input.length;
         
         if (input[i] >= scoreObject.highest){
-            scoreObject.highest = input[i]
+            scoreObject.highest = input[i];
         }
         if (input[i] <= scoreObject.lowest) {
-            scoreObject.lowest = input[i]
+            scoreObject.lowest = input[i];
         }
     }
     if (scoreObject.average > 60){
-        scoreObject.passing = true
+        scoreObject.passing = true;
     }
-    console.log(scoreObject)
-    return scoreObject
+    console.log(scoreObject);
+    return scoreObject;
 }
 
